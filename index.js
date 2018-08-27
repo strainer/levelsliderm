392c17e036859ac59b50985be41d141bce8d9430
andrews <andrewInput@gmail.com>
2018-08-27 16:54:36 +0100

    sanding

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
 var cmin=0,cmax=100
 var phob={a:20}
 
+var slength="2.75em",swide="20em"
+
+var s ={ 
+  stateobj:phob ,statekey:"a" 
+ ,min:cmin ,max:cmax ,steps:100
+ ,elstyle:{ border:"0px dashed black",width:slength,height:swide,float:"left" }
+ ,horizontal:false
+ ,knobsize:"4.9%"
+ ,knobcolor:"grey"
+ ,strokecolor:"dimgrey"
+ ,knobstrokecolor:"black"
+ ,strokewidth:"0.8%"
+ ,knobstrokewidth:"0.8%"
+ 
+}
+
+var d=document.getElementById("dash")
+var b=document.createElement("div")
+d.appendChild(b)
+
+appendslider(s,b)
+s.reverse=true
+appendslider(s,b)
+s.reverse=false
+b=document.createElement("div")
+d.appendChild(b)
+
+s.horizontal=true
+s.elstyle.width=swide;
+s.elstyle.height=slength;
+//slength;
+for(var i=0;i<4;i++) appendslider(s,b)
+s.reverse=true
+s.elstyle.height="1.6em"
+for(var i=0;i<12;i++) appendslider(s,b)
+s.reverse=false
+for(var i=0;i<6;i++) appendslider(s,b)
+
+function appendslider(s,e){ 
+  var b = document.createElement("div")
+  //d=e||d||document.body
+  
+  e.appendChild(b)
+  m.mount(b, 
+    {
+      view: function () {
+        return m("div",
+          m( levelsliderm, s )
+        ) 
+      }
+    }
+  )
+}
+
+
+/*
+
 m.mount(document.getElementById("domb3"), {
 view: function () {
   
@@ -10,56 +67,5 @@ view: function () {
     ]
   )
 }
-} )
-
-addslider()
-
-function addslider(){
-  //var pot=Frdandom.pot()
-  
-  var d = document.createElement("div")
-  document.body.appendChild(d)
-  m.mount(d, 
-    {
-      view: function () {
-        return m("div",
-          m( levelsliderm, { 
-              stateobj:phob ,statekey:"a" 
-             ,min:cmin ,max:cmax ,steps:100
-             ,elstyle:{border:"1px solid black",width:"25em",height:"5em"} 
-            } )
-        ) 
-      }
-    }
-  )
-}
-
-
-
-m.mount(document.getElementById("domb3"), {
-  view: function () {
-    
-    return m("div",
-      [
-        m( ccomponent, { stateobj:phob ,statekey:"a" } ),
-        //~ m( levelsliderm, { 
-          //~ stateobj:phob ,statekey:"a" 
-         //~ ,min:cmin ,max:cmax ,steps:100
-         //~ ,elstyle:{border:"1px solid black",width:"25em",height:"5em"} 
-        //~ } ),
-        m( levelsliderm, { 
-          stateobj:phob ,statekey:"a" 
-         ,min:cmin ,max:cmax ,steps:100
-         ,elstyle:{border:"1px solid black",width:"25em",height:"5em"} 
-        } ),
-        m( levelsliderm, { 
-          stateobj:phob ,statekey:"a", vertical:true 
-         ,min:cmin ,max:cmax ,steps:100
-         ,elstyle:{border:"1px solid black"} 
-        } ),
-        
-      ]
-    )
-  }
 } )
 */
